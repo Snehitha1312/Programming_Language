617824a6dab5e7f7b4c8ca8b73bcab8627d0a274
Snehitha1312 <psnehitha3333@gmail.com>
2025-09-29 19:20:07 +0530

    Fixed some errors

--- texteditor.cpp
+++ texteditor.cpp
@@ -53,22 +53,30 @@ void saveFile() {
         out << E.rows[i];
         if (i + 1 < E.rows.size()) out << "\n";
     }
+    E.status_msg = "\""+E.filename+"\" written";
 }
-
-
 
 void drawRows() {
     cout << "\x1b[2J"; // Clear screen
     cout << "\x1b[H";  // Move cursor to top-left
 
     for (size_t i = 0; i < E.rows.size(); i++) {
-        cout << E.rows[i] << "\r\n";
+        cout << i+1 << " " << E.rows[i] << "\r\n";
     }
 
     // Status line
     cout << "\x1b[7m"; // Inverse colors
     cout << "FILE: " << (E.filename.empty() ? "[No Name]" : E.filename)
-         << " | MODE: " << (E.insert_mode ? "INSERT" : "NORMAL") << "\x1b[m\r\n";
+         << " | MODE: " << (E.insert_mode ? "INSERT" : (E.command_mode ? "COMMAND" : "NORMAL"))
+         << "\x1b[m\r\n";
+
+    // Command / message line
+    if (E.command_mode) {
+        cout << ":" << E.command_buffer;
+    } else {
+        cout << E.status_msg;
+    }
+    cout << "\r\n";
 }
 
 void moveCursor(char key) {
@@ -84,6 +92,7 @@ void insertChar(char c) {
     E.rows[E.cy].insert(E.rows[E.cy].begin() + E.cx, c);
     E.cx++;
 }
+
 void deleteChar() {
     if (E.cx < E.rows[E.cy].size()) {
         E.rows[E.cy].erase(E.cx,1);
@@ -98,31 +107,6 @@ void processCommand() {
 
     E.command_buffer.clear();
     E.command_mode = false;
-}
-
-    int c = readKey();
-
-    if (!E.insert_mode) {
-        switch (c) {
-            case 'i': E.insert_mode = true; break;
-            case 'q': exit(0); break;
-            case 's': saveFile(); break;
-            default: moveCursor(c);
-        }
-    } else {
-        if (c == 27) { // ESC
-            E.insert_mode = false;
-        } else if (c == 127 || c == '\b') { // Backspace
-            if(E.cx>0) { E.rows[E.cy].erase(E.cx-1,1); E.cx--; }
-        } else if (c == '\r') { // Enter
-            string newLine = E.rows[E.cy].substr(E.cx);
-            E.rows[E.cy] = E.rows[E.cy].substr(0, E.cx);
-            E.rows.insert(E.rows.begin() + E.cy + 1, newLine);
-            E.cy++; E.cx = 0;
-        } else {
-            insertChar(c);
-        }
-    }
 }
 
 void processKeypress() {
@@ -172,15 +156,15 @@ void processKeypress() {
     }
 }
 
-
-
 int main(int argc, char* argv[]) {
     if (argc >= 2) openFile(argv[1]);
+    else E.rows.push_back("");
+
     enableRawMode();
 
     while (true) {
         drawRows();
-        cout << "\x1b[" << E.cy+1 << ";" << E.cx+1 << "H"; // Move cursor
+        cout << "\x1b[" << E.cy+1 << ";" << E.cx+3 << "H"; // Adjust cursor (2 chars for line numbers + space)
         cout.flush();
         processKeypress();
     }
